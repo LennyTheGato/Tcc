3ee16cca9e74fca855262c5caec6da4dba0ea091
LennyTheGato <novelodepao@gmail.com>
2020-11-12 19:33:11 -0300

    João Pedro

---
+++ Tcc/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Medicina da Terra</title>
+
+<meta charset="utf-8">
+<meta name="generator" content="Bluefish 2.2.10" >
+<meta name="author" content="C13834" >
+<meta name="date" content="2020-03-10T15:01:17-0300" >
+<meta name="copyright" content="">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="expires" content="0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="stylesheet" href="css/css.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+
+<script>
+  function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+  }
+  window.addEventListener('load', addDarkmodeWidget);
+</script>
+
+  <title>Medicina da Terra</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+   <!-- Custom styles for this template -->
+   <link href="css/full-width-pics.css" rel="stylesheet">
+   
+<script type="text/javascript" src="jquery-3.4.1.js" > </script>
+
+
+
+<script type="text/javascript">
+
+	function carregar () {
+		$('#nav').load( 'nav.php' );
+		$('#content').load( 'content.php' );
+        $('#footer').load( 'footer.php' );
+        $('#header').load( 'header.php' );
+	}
+
+</script>
+
+</head>
+<body onload=" carregar() ">
+
+  <!-- Header - set the background image for the header in the line below -->
+  <header class="py-5 bg-image-full" style="background-image: url('https://cdn.suwalls.com/wallpapers/abstract/green-fractal-44737-2560x1440.jpg');">
+    <img class="img-fluid d-block mx-auto" src="images/mt.png" alt="" width="320" height="205">
+  </header>
+	<div id="nav" class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">InnerHTML</div>
+	<div id="content">InnerHTML</div>
+	<div id="footer">InnerHTML</div>
+	
+	  <!-- Bootstrap core JavaScript -->
+	  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
