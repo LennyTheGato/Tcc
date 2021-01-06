8764173d4d3d6afb972c912ba01745c89b314ecf
MarianaElma <62039731+MarianaElma@users.noreply.github.com>
2021-01-06 17:00:53 -0300

    Update curso_database.sql

--- Tcc/crud/Cursos/curso_database.sql
+++ Tcc/crud/Cursos/curso_database.sql
@@ -1,11 +1,19 @@
 use medicinadaterra;
 
+CREATE TABLE cursos(
+	idCurso		int not null auto_increment,
+	nomeCurso	varchar(128) not null,
+    preço			int not null,	
+	texto			text not null,
+    linkCurso		varchar(255) not null,
+    PRIMARY KEY (idCurso)
+);
+
 CREATE TABLE messages(
 	idMessage		int not null auto_increment,
 	emailRemetente	varchar(128) not null,
     assunto			varchar(255) not null,	
 	texto			text,
     dthrEnvio		datetime,
-    lido			int,
     PRIMARY KEY (idMessage)
 );
